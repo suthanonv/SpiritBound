ae1ad04f577951d2038ba6c652c0a113fac4ee33
suthanonv <122354357+suthanonv@users.noreply.github.com>
2024-07-10 15:54:29 +0700

    improved Room Generate Script

--- Assets/Script/Room.cs
+++ Assets/Script/Room.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class Room : MonoBehaviour
 {
     [SerializeField] Transform PlayerSpawnPosition;
     [SerializeField] Transform CameraHolderPositon;
+
+    [SerializeField] List<FormStageComponent> ElementInEachForm = new List<FormStageComponent>();
      public void SetRoom(Transform Player)
     {
+        RoomDestination.instance.RoomThatPlayerin = this;
         Player.transform.position = PlayerSpawnPosition.position;
+    }
+
+
+   
+    public void SetRoomElement(PlayerFormState CurrentState)
+    {
+        List<GameObject> ObjectToOpen = new List<GameObject>();
+
+        FormStageComponent componenet = ElementInEachForm.FirstOrDefault(i => i.formState == CurrentState);
+
+        foreach (GameObject i in componenet.ObjectInFormStage) i.SetActive(true);
 
     }
 
+    public void CloseRoomELement(PlayerFormState Opposit)
+    {
+        List<GameObject> ObjectToClose = new List<GameObject>();
+
+        FormStageComponent componenet = ElementInEachForm.FirstOrDefault(i => i.formState == Opposit);
+
+        foreach (GameObject i in componenet.ObjectInFormStage) i.SetActive(false);
+    }
 }
+
+
