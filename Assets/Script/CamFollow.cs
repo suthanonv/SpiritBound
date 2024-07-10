bd525558ba6912dc7273dd9172537ef321571bbb
Pupa-Thapana <37727@virtual.prc.ac.th>
2024-07-10 10:42:37 +0700

    attack

--- Assets/Script/CamFollow.cs
+++ Assets/Script/CamFollow.cs
@@ -13,6 +13,6 @@ public class CamFollow : MonoBehaviour
         Vector3 desiredPosition = player.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-    }
+    } 
   
 }
